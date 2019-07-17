4560da73ad00e2f2f8f22f9722a82bdd11ba5f2f
BALURAAM <52704338+BALURAAM@users.noreply.github.com>
2019-07-17 14:36:19 +0530

    Create 48.py

---
+++ 48.py
@@ -0,0 +1,11 @@
+bala=input()
+num = len(bala)
+if num > 1:
+   for i in range(2,num):
+       if (num % i) == 0:
+           print("no")
+           break
+   else:
+       print("yes")
+else:
+   print("no")
