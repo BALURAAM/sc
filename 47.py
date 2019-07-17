44bc264080e8cde92e88320798e92f402e68c512
BALURAAM <52704338+BALURAAM@users.noreply.github.com>
2019-07-17 14:54:50 +0530

    Update 47.py

--- 47.py
+++ 47.py
@@ -1,19 +1,19 @@
-
-def computeHCF(x, y):
-    if x > y:
-        smaller = y
-    else:
-        smaller = x
-    for i in range(1, smaller+1):
-        if((x % i == 0) and (y % i == 0)):
-            hcf = i
-            
-    return hcf
-a, b = list(map(int,input().split()))
-num1=1
-for i in range(1,a+1):
-    num1=num1*i
-num2=1
-for i in range(1,b+1):
-    num2=num2*i
-print(computeHCF(num1, num2))
+import math
+bala, b = list(map(int,input().split()))
+if bala <= 100:
+  bala = math.factorial(bala)
+else:
+  bala = 0
+if b <= 100:
+  b = math.factorial(b)
+else:
+  b = 0
+while bala != 0 and b != 0:
+  if bala > b:
+    bala -= b
+  else:
+    b -= bala
+if bala == 0:
+  print(b)
+else:
+  print(b)
