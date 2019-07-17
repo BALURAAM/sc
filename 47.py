671c21f7c740f39141d4b3d9c3da4cd24b3db7d4
BALURAAM <52704338+BALURAAM@users.noreply.github.com>
2019-07-17 14:50:17 +0530

    Update 47.py

--- 47.py
+++ 47.py
@@ -9,11 +9,10 @@ def computeHCF(x, y):
             hcf = i
             
     return hcf
-a=int(input())
+a, b = list(map(int,input().split()))
 num1=1
 for i in range(1,a+1):
     num1=num1*i
-b=int(input())
 num2=1
 for i in range(1,b+1):
     num2=num2*i
